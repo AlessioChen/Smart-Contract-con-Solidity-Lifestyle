66614e2ac367fca0449e65b1605d77593c7bc43e
Alessio Chen <alessiochen98@gmail.com>
2023-05-06 01:29:24 +0200

    feat: several updates

--- LoadManager.sol
+++ LoadManager.sol
@@ -3,35 +3,32 @@ pragma solidity ^0.8.0;
 contract LoanManager {
 
     enum LoanState{
-        Requested,
         Active,
         Paid,
-        Expired,
+        Late,
+        Defaulted,
         Cancelled
     }
 
-    enum PayBackInterval {
-        fifteenDays,
-        oneMonth
-    }
 
     struct Loan {
         address borrower;
-        address lender;
+        address payable lender;
+        uint amount;
         uint256 interestRate;
+        uint duration;
         uint256 startDate;
-        uint256 dueDate;
-        uint256 amount;
-        uint256 amountPaid;
-        PayBackInterval interval;
+        uint256 endDate;
+        uint256 totalAmount;
+        uint256 remainingAmount;
         LoanState state;
     }
 
-    mapping (uint256 => Loan) public loans;
-    uint256 public loanCounter;
+    Loan[] public loans;
 
-    modifier onlyLoanExists(uint256 loanId) {
-        require(loanId > 0 && loanId <= loanCounter, "Loan does not exist");
+
+    modifier onlyExistLoan(uint _loanId) {
+        require(_loanId < loans.length, "Invalid loan ID");
         _;
     }
 
@@ -51,45 +48,75 @@ contract LoanManager {
     }
 
 
-    function requestLoan(address _lender, uint256 _amount, uint256 _interestRate, uint256 _duration, PayBackInterval interval) public{
-        require(_amount > 0, "Loan amount must be grater than zero.");
-        require(_interestRate > 0, "Interest rate must be grater than zero.");
 
-        uint256 endTime = block.timestamp + _duration;
-        loans[loanCounter] = Loan(
-            msg.sender,
-            _lender,
-            _interestRate,
-            block.timestamp,
-            endTime,
-            0,
-            0,
-            interval,
-            LoanState.Requested
-        );
+    function createLoan(address _borrower, uint _interestRate, uint _duration) public payable{
+        require(_borrower != address(0), "Invalid borrower address");
+        require(msg.value > 0, "Loan amount must be greater than zero");
+        require(_interestRate > 0, "Interest rate must be greater than zero");
+        require(_duration > 0, "Loan duration must be greater than zero");
 
-        loanCounter++;
+        uint totalAmount = msg.value + ((msg.value * _interestRate) / 100);
+        uint startTime = block.timestamp;
+        uint endTime = startTime + (_duration * 1 days);
+
+        loans.push(Loan({
+            borrower: _borrower,
+            lender: payable(msg.sender),
+            amount: msg.value,
+            interestRate: _interestRate,
+            duration: _duration,
+            startDate: startTime,
+            endDate: endTime,
+            totalAmount: totalAmount,
+            remainingAmount: totalAmount,
+            state: LoanState.Active
+        }));
 
     }
 
-    function cancelLoan(uint _loanId) public onlyLoanExists(_loanId) onlyValidState(_loanId, LoanState.Requested)  onlyBorrower(_loanId) {
+    function cancelLoan(uint _loanId) public onlyExistLoan(_loanId) onlyValidState(_loanId, LoanState.Active)  onlyBorrower(_loanId) {
         loans[_loanId].state = LoanState.Cancelled;
     }
 
-    function approveLoan(uint256 _loanId) public payable onlyLoanExists(_loanId) onlyValidState(_loanId, LoanState.Requested) onlyLender(_loanId){
-        require(msg.value == loans[_loanId].amount, "Amount sent must match loan amount.");
-        loans[_loanId].state = LoanState.Active;
+    function repayLoan(uint _loanId) public payable onlyValidState(_loanId, LoanState.Active) onlyExistLoan(_loanId)  onlyBorrower(_loanId){
+
+        require(msg.value > 0 && msg.value <= loans[_loanId].remainingAmount, "Invalid repayment amount");
+
+        uint penalty = 0;
+        if (block.timestamp > loans[_loanId].endDate) {
+            penalty = ((loans[_loanId].totalAmount * 10) / 100); // penalty of 10% of the total amount
+            loans[_loanId].state = LoanState.Defaulted;
+        } else if (block.timestamp > (loans[_loanId].endDate - 1 days)) {
+            penalty = ((loans[_loanId].totalAmount * 5) / 100); // penalty of 5% of the total amount
+            loans[_loanId].state = LoanState.Late;
+        }
+
+        uint amountToRepay = msg.value;
+        if(penalty> 0){
+            amountToRepay += penalty;
+        }
+
+        loans[_loanId].remainingAmount -= msg.value;
+
+        if(loans[_loanId].remainingAmount <= 0){
+            loans[_loanId].state = LoanState.Paid;
+        }
+
+        loans[_loanId].lender.transfer(amountToRepay);
+
     }
 
-    function getLoan(uint256 _loanId) public view onlyLoanExists(_loanId) returns(
+
+    function getLoan(uint256 _loanId) public view onlyExistLoan(_loanId) returns(
         address borrower,
         address lender,
+        uint amount,
         uint256 interestRate,
+        uint duration,
         uint256 startDate,
-        uint256 dueDate,
-        uint256 amount,
-        uint256 amountPaid,
-        PayBackInterval interval,
+        uint256 endDate,
+        uint256 totalAmount,
+        uint256 remainingAmount,
         LoanState state
 
     ){
@@ -98,12 +125,12 @@ contract LoanManager {
         borrower = loan.borrower;
         lender = loan.lender;
         amount = loan.amount;
+        duration = loan.duration;
         interestRate = loan.interestRate;
         startDate = loan.startDate;
-        dueDate = loan.dueDate;
-        amount = loan.amount;
-        amountPaid = loan.amountPaid;
-        interval = loan.interval;
+        endDate = loan.endDate;
+        totalAmount = loan.totalAmount;
+        remainingAmount = loan.remainingAmount;
         state = loan.state;
 
     }
