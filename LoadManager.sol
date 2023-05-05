d8932f86d4f41e1b73efbc71fd874196b8eb6c02
Alessio Chen <alessiochen98@gmail.com>
2023-05-06 00:46:48 +0200

    feat: add modifiers and getLoan

--- LoadManager.sol
+++ LoadManager.sol
@@ -18,13 +18,31 @@ contract LoanManager {
         uint256 dueDate;
         uint256 amount;
         uint256 amountPaid;
-        bool isPaid;
-        bool isCancelled;
         LoanState state;
     }
 
     mapping (uint256 => Loan) public loans;
     uint256 public loanCounter;
+
+    modifier onlyExistLoan(uint256 loanId) {
+        require(loanId > 0 && loanId <= loanCounter, "Loan does not exist");
+        _;
+    }
+
+    modifier onlyValidState(uint256 loanId, LoanState state) {
+        require(loans[loanId].state == state, "Invalid loan state");
+        _;
+    }
+
+    modifier onlyBorrower(uint256 loanId) {
+        require(msg.sender == loans[loanId].borrower, "Only borrower can call this function");
+        _;
+    }
+
+    modifier onlyLender(uint256 loanId) {
+        require(msg.sender == loans[loanId].lender, "Only lender can call this function");
+        _;
+    }
 
 
     function requestLoan(address _lender, uint256 _amount, uint256 _interestRate, uint256 _duration) public{
@@ -40,8 +58,6 @@ contract LoanManager {
             endTime,
             0,
             0,
-            false,
-            false,
             LoanState.Requested
         );
 
@@ -49,20 +65,40 @@ contract LoanManager {
 
     }
 
-    function cancelLoan(uint _loanId) public{
-        require(loans[_loanId].state == LoanState.Requested, "Loan is not in requested state.");
-        require(loans[_loanId].borrower == msg.sender, "Only borrower can cancel the loan.");
-
+    function cancelLoan(uint _loanId) public onlyExistLoan(_loanId) onlyValidState(_loanId, LoanState.Requested)  onlyBorrower(_loanId) {
         loans[_loanId].state = LoanState.Cancelled;
     }
 
-    function approveLoan(uint256 _loanId) public payable{
-        require(loans[_loanId].state == LoanState.Requested, "Loan is not in requested state.");
-        require(loans[_loanId].lender == msg.sender, "Only lender can approve the loan.");
+    function approveLoan(uint256 _loanId) public payable onlyExistLoan(_loanId) onlyValidState(_loanId, LoanState.Requested) onlyLender(_loanId){
         require(msg.value == loans[_loanId].amount, "Amount sent must match loan amount.");
-
         loans[_loanId].state = LoanState.Active;
     }
+
+    function getLoan(uint256 _loanId) public view onlyExistLoan(_loanId) returns(
+        address borrower,
+        address lender,
+        uint256 interestRate,
+        uint256 startDate,
+        uint256 dueDate,
+        uint256 amount,
+        uint256 amountPaid,
+        LoanState state
+
+    ){
+
+        Loan storage loan = loans[_loanId];
+        borrower = loan.borrower;
+        lender = loan.lender;
+        amount = loan.amount;
+        interestRate = loan.interestRate;
+        startDate = loan.startDate;
+        dueDate = loan.dueDate;
+        amount = loan.amount;
+        amountPaid = loan.amountPaid;
+        state = loan.state;
+
+    }
+
 
 
 }
